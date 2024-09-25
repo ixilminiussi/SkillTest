b0b21d97451bc2096a47a0b85c5c50154364ae1e
Thomas Vinadelle <35263641+Zarechi@users.noreply.github.com>
2024-09-25 16:37:12 +0200

    Cours du 25/09/2024
    - Instanciation du GameplayWorkflow
    - Instanciation des Controles (Move + Sprint + Crouch)

---
+++ Source/SkillTest_GD3/Public/SkillTestPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SkillTestPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKILLTEST_GD3_API ASkillTestPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
